e5321e6fcad8a7799ba0afbdeaf001d336bfad6e
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2022-12-30 15:26:08 +0100

    first commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author david
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
